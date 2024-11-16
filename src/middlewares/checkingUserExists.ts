412ff3e35697e99b54fea63c79eae756c75f54c6
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 14:43:53 -0300

    feat: Creating middleware to check if the user exists

---
+++ src/middlewares/checkingUserExists.ts
@@ -0,0 +1,31 @@
+//Importações
+import { Request, Response, NextFunction } from "express";
+import { validate } from "uuid";
+
+//Services
+import { findUserDataByID } from "../services/userService";
+
+//Definindo o middleware
+export async function checkingUserExists(req:Request, res: Response, next: NextFunction) {
+    //Pegando o id do usuário
+    const userId = req.params.userId;
+    
+    try {
+        //Verificando se o atributo foi passado
+        if (!userId || userId === null || userId === undefined) return res.status(401).json({ message: "ID of user not informed" });
+
+        //Verificando se o atributo é um ID válido
+        if (!validate(userId)) return res.status(400).json({ message: "Invalid format ID" });
+
+        //Verificando se o usuário existe
+        const userExistWithID = await findUserDataByID(userId);
+        if (!userExistWithID) return res.status(404).json({ message: "User not found" });
+
+        // Usuário encontrado, seguindo para a próxima função
+        next();
+
+    } catch (error) {
+        console.error("Error checking user existence:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    };
+};
