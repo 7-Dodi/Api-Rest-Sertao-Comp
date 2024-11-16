4de7e5177c4ef1203445a12817602ee5b96a6475
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 15:15:39 -0300

    refactor: Updating types of user

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,10 +1,16 @@
 //Definindo interface padrão para usuário
 export interface UserDTO {
-    id?: string;
+    id: string;
     name: string;
     email: string;
-    password?: string;
-    profileImage?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+    password: string;
+    profileImage: string;
+    createdAt: Date;
+    updatedAt: Date;
 };
+
+//Criando tipo para a criação de um usuário
+export type createUserDTO = Omit <UserDTO, "id" | "profileImage" | "createdAt" | "updatedAt">
+
+//Criando tipo para edição de um usuário
+export type updadeUserDTO = Omit<createUserDTO, 'password'>
