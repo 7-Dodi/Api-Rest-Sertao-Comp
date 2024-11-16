564fbb18bcb0baf24929a7fa5bb8be3b563bc516
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 14:24:57 -0300

    feat: Creating a service to list user data by ID

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -8,3 +8,12 @@ export const findAllUsers = async () => {
     const allUsers = await prisma.user.findMany();
     return allUsers;
 };
+
+//Método para listar um usuáiro por ID
+export const findUserDataByID = async (userId: string) => {
+    const userData = await prisma.user.findUnique({
+        where: {id: userId}
+    });
+
+    return userData;
+};
