35321cf24b430380c9fe1a8d2176bab28def0fcf
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 15:17:22 -0300

    feat: Creating a route to create users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,13 +2,14 @@
 import { Router } from "express";
 
 //Controllers
-import { listAllUsers, listUserDataByID } from "../controllers/userController";
+import { listAllUsers, listUserDataByID, postCreateUser } from "../controllers/userController";
 
 //Configurações
 const userRoutes = Router();
 
 //Definindo rotas
 userRoutes.get("/", listAllUsers);
+userRoutes.post("/", postCreateUser);
 userRoutes.get("/:userId", listUserDataByID);
 
 //Exportando
