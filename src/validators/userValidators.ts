9242badf928dc28baae30fc9f7f1fa3d38abdcf3
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 14:36:54 -0300

    feat: Creating validations for editing user information

--- src/validators/userValidators.ts
+++ src/validators/userValidators.ts
@@ -6,10 +6,20 @@ export const createUserSchema = z.object({
     name: z.string()
         .min(3, { message: 'Name must be at least 3 characters long' })
         .max(50, { message: 'Name must be at most 50 characters long' }),
-    
+
     email: z.string()
         .email({ message: 'Please enter a valid email address' }),
 
     password: z.string()
         .min(5, { message: 'Password must be at least 5 characters long' }),
+});
+
+//Validaçõa para edição das informações de um usuário
+export const updateUserDataSchema = z.object({
+    name: z.string()
+        .min(3, { message: 'Name must be at least 3 characters long' })
+        .max(50, { message: 'Name must be at most 50 characters long' }),
+
+    email: z.string()
+        .email({ message: 'Please enter a valid email address' }),
 });
