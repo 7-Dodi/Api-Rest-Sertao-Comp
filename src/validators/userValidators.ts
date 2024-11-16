980a4e3bce5b8fdd286750a53a2747a80af983c6
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 14:35:08 -0300

    feat: Creating user validations

---
+++ src/validators/userValidators.ts
@@ -0,0 +1,15 @@
+//Importações
+import { z, ZodError } from "zod";
+
+//Validação para criação de um usuário
+export const createUserSchema = z.object({
+    name: z.string()
+        .min(3, { message: 'Name must be at least 3 characters long' })
+        .max(50, { message: 'Name must be at most 50 characters long' }),
+    
+    email: z.string()
+        .email({ message: 'Please enter a valid email address' }),
+
+    password: z.string()
+        .min(5, { message: 'Password must be at least 5 characters long' }),
+});
