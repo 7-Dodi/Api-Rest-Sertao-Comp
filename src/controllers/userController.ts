db408d55633a2e84754d6edb83c4c669b95f8797
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-16 14:24:34 -0300

    feat: Creating a controller to list user data by ID

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,9 @@
 //Importações
 import { Request, Response } from "express";
+import { validate } from "uuid";
 
 //Services
-import { findAllUsers } from "../services/userService";
+import { findAllUsers, findUserDataByID } from "../services/userService";
 
 //Definindo as requisições
 
@@ -19,5 +20,27 @@ export const listAllUsers = async (req: Request, res: Response): Promise<Respons
     } catch (error) {
         console.error("Error in return datas of users: ", error);
         return res.status(500).json({error: "Error in return datas of users"});
+    };
+};
+
+//Requisição para listar usuário por ID
+export const listUserDataByID = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        //Buscar dados na requisição
+        const userId = req.params.userId;
+
+        //Verificar se o id é válido
+        if (!userId || userId === null || userId === undefined) return res.status(400).json({error: "User ID not informed"});        
+        if (!validate(userId)) return res.status(400).json({error: "Invalid ID format"});
+
+        //Busca dados no service
+        const userData = await findUserDataByID(userId);
+        if(!userData) return res.status(404).json({message: "User not found"});
+
+        //Retornando dado final
+        return res.status(200).json({message: "User found, listing data", data: userData});
+    } catch (error) {
+        console.error("Error in return data of user by ID: ", error);
+        return res.status(500).json({error: "Error in return data of user by ID"});
     };
 };
